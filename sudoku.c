e7000c8a048f5e16d0303f6003b689ce00014563
JoseRosell <6367213@alu.murciaeduca.es>
2025-01-29 12:53:37 +0100

    Add initial Sudoku game implementation and build task configuration

---
+++ sudoku.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int tablero[9][9] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+
+void rellenarTablero(int tablero[9][9]){
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++){
+            printf("Ingrese el valor de la casilla [%d][%d]: ", i, j);
+            scanf("%d", &tablero[i][j]);
+        }
+    }
+}
+
+void imprimirTablero(int tablero[9][9]){
+    // Imprimir tablero formateado con tabulaciones y |
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++){
+            printf("%d\t", tablero[i][j]);
+            if (j == 2 || j == 5){
+                printf("| ");
+            }
+        }
+        printf("\n");
+        if (i == 2 || i == 5){
+            printf("---------------------\n");
+        }
+    }
+
+int main(){
+    rellenarTablero(tablero[9][9]);
+    imprimirTablero(tablero[9][9]);
+        return 0;
+    }
+}
+
+
